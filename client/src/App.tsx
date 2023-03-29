899d469241ba87b1b53f5c4459ab7fb2ffee0db7
Ayush Jain <77970015+not4YU5H@users.noreply.github.com>
2023-03-30 02:46:23 +0530

    Material UI config and theme done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+
 function App() {
-  return <div className="app"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), [])
+  return <div className="app">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      Hi!
+    </ThemeProvider>
+  </div>;
 }
 
 export default App
